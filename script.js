c56149520b7b08589a40fd999f5cf82b058a99b2
Konst_V <varjutin@yandex.ru>
2022-03-21 19:50:07 +0300

    Переделал все

--- script.js
+++ script.js
@@ -2,23 +2,13 @@
 
 const swiper = new Swiper('.swiper', {
     // Optional parameters
-    direction: 'horizontal',
     speed: 600,
     spaceBetween: 16,
-    grabCursor: true,
     slidesPerView: 'auto',
-    centeredSlides: true,
-    centeredSlidesBounds: true,
-    slideToClickedSlide: true,
-    watchOverFlow: true,
-    watchSlidesProgress: true,
-    freeMode: true,
-    obserever: true,
     pagination: {
         el: '.swiper-pagination',
         type: 'bullets',
         clickable: true,
-        dynamicMainBullets: 7,
     },
     mousewheel: {
         invert: true,
@@ -31,10 +21,12 @@ const swiper = new Swiper('.swiper', {
 
 });
 
+
+
 document.querySelector('.btn-next--brands').addEventListener('click', function (){
     let panel = this.previousElementSibling;
     if (panel.clientHeight > 176) {
-    this.innerHTML = ' <img class="btn-next__icon" src="images/expand.svg" alt="Двойная стрелка вниз">Показать все';
+        this.innerHTML = ' <img class="btn-next__icon" src="images/expand.svg" alt="Двойная стрелка вниз">Показать все';
         panel.style.maxHeight = "176px";
     } else {
         panel.style.maxHeight = panel.scrollHeight + "px";
