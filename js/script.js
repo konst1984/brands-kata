20df8202a8122e8f8cf73028b0eb24934ef3b60d
Konst_V <varjutin@yandex.ru>
2022-03-27 13:48:41 +0300

    Форматирование 2.0

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-"use strict"
+"use strict";
 
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
     // Optional parameters
     speed: 600,
     spaceBetween: 16,
-    slidesPerView: 'auto',
+    slidesPerView: "auto",
     pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
+        el: ".swiper-pagination",
+        type: "bullets",
         clickable: true,
         // dynamicBullets: true,
         // dynamicMainBullets: 7,
@@ -22,7 +22,7 @@ const swiper = new Swiper('.swiper', {
     },
 });
 
-document.querySelector('.btn-next--brands').addEventListener('click', function() {
+document.querySelector(".btn-next--brands").addEventListener("click", function () {
     let panel = this.previousElementSibling;
     if (panel.clientHeight > 176) {
         this.innerHTML = ' <img class="btn-next__icon" src="../assets/images/expand.svg" alt="Двойная стрелка вниз">Показать все';
@@ -31,4 +31,4 @@ document.querySelector('.btn-next--brands').addEventListener('click', function()
         panel.style.maxHeight = panel.scrollHeight + "px";
         this.innerHTML = ' <img class="btn-next__icon" src="../assets/images/expand-up.svg" alt="Двойная стрелка вверх">Скрыть';
     }
-})
+});
