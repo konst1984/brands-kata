6e9314b306a11aac84bd50825ff911f3da76ec20
Konst_V <varjutin@yandex.ru>
2022-03-27 15:04:58 +0300

    prettier заработал

--- js/script.js
+++ js/script.js
@@ -1,34 +1,38 @@
 "use strict";
 
 const swiper = new Swiper(".swiper", {
-    // Optional parameters
-    speed: 600,
-    spaceBetween: 16,
-    slidesPerView: "auto",
-    pagination: {
-        el: ".swiper-pagination",
-        type: "bullets",
-        clickable: true,
-        // dynamicBullets: true,
-        // dynamicMainBullets: 7,
-    },
-    mousewheel: {
-        invert: true,
-    },
-    keyboard: {
-        enabled: true,
-        onlyInViewport: true,
-        pageUpDown: true,
-    },
+  // Optional parameters
+  speed: 600,
+  spaceBetween: 16,
+  slidesPerView: "auto",
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+    // dynamicBullets: true,
+    // dynamicMainBullets: 7,
+  },
+  mousewheel: {
+    invert: true,
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
 });
 
-document.querySelector(".btn-next--brands").addEventListener("click", function () {
+document
+  .querySelector(".btn-next--brands")
+  .addEventListener("click", function () {
     let panel = this.previousElementSibling;
     if (panel.clientHeight > 176) {
-        this.innerHTML = ' <img class="btn-next__icon" src="../assets/images/expand.svg" alt="Двойная стрелка вниз">Показать все';
-        panel.style.maxHeight = "176px";
+      this.innerHTML =
+        ' <img class="btn-next__icon" src="../assets/images/expand.svg" alt="Двойная стрелка вниз">Показать все';
+      panel.style.maxHeight = "176px";
     } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-        this.innerHTML = ' <img class="btn-next__icon" src="../assets/images/expand-up.svg" alt="Двойная стрелка вверх">Скрыть';
+      panel.style.maxHeight = panel.scrollHeight + "px";
+      this.innerHTML =
+        ' <img class="btn-next__icon" src="../assets/images/expand-up.svg" alt="Двойная стрелка вверх">Скрыть';
     }
-});
+  });
